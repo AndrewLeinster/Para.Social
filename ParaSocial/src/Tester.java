87f50874756b447363c14996ad126a8402c2ab7e
AndrewLeinster <2456280@dundee.ac.uk>
2023-03-24 14:45:56 +0000

    Renaming image files

--- ParaSocial/src/Tester.java
+++ ParaSocial/src/Tester.java
@@ -23,13 +23,12 @@ public class Tester {
         main.writeToFile();
     }
 
-    public void addUsers()
-    {
-        User user1 = new User("Laura", "1", "Starbucks", "Glenrothes", "Images/PFPs/1ALP0101.jpg", new ArrayList<String>());
-        User user2 = new User("Adam", "2", "O2", "Dunfermline", "Images/PFPs/1ALP0209.jpg", new ArrayList<String>());
-        User user3 = new User("Iona", "3", "Tesco", "Monifieth", "Images/PFPs/1ALP0265.jpg", new ArrayList<String>());
-        User user4 = new User("Andrew", "4", "Self-Employed", "Fife", "Images/PFPs/1ALP02429.jpg", new ArrayList<String>());
-        User user5 = new User("Marcus", "5", "Old Course", "Monikie", "Images/PFPs/1ALP1004.jpg", new ArrayList<String>());
+
+        User user1 = new User("Laura", "1", "Starbucks", "Glenrothes", "Images/PFPs/Laura.jpg", new ArrayList<String>());
+        User user2 = new User("Adam", "2", "O2", "Dunfermline", "Images/PFPs/Adam.jpg", new ArrayList<String>());
+        User user3 = new User("Iona", "3", "Tesco", "Monifieth", "Images/PFPs/Iona.jpg", new ArrayList<String>());
+        User user4 = new User("Andrew", "4", "Self-Employed", "Fife", "Images/PFPs/Andrew.jpg", new ArrayList<String>());
+        User user5 = new User("Marcus", "5", "Old Course", "Monikie", "Images/PFPs/Marcus.jpg", new ArrayList<String>());
         
         user1.addFriend(user2.getID());
         user1.addFriend(user3.getID());
